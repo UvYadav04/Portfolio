a549f0e9344d9ada458159dc7a9380818867c1b1
Dinesh Yadav <dineshnirban01@gmail.com>
2025-01-12 17:44:56 +0530

    renamed screen folder

--- frontend2/src/components/NewBox.jsx
+++ frontend2/src/components/NewBox.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function NewBox() {
     return (
         <div className='w-[80%] bg-red-600'>
-            hello buddy
+            {/* hello buddy */}
         </div>
     )
 }
--- frontend2/src/components/Projects.jsx
+++ frontend2/src/components/Projects.jsx
@@ -41,7 +41,6 @@ export default function Projects() {
     const [index, setindex] = useState(0)
     return (
         <div className='propage container-fluid  p-0 mt-md-5 mt-5 pt-lg-5 pt-md-5 pt-sm-3 pt-0 pb-lg-3 pb-sm-5 pb-2 '>
-            <NewBox />
             <div className="text-center py-3 mb--3 mb-0 text-white"><h1>RECENT PROJECTS</h1></div>
             <div className="row d-lg-flex d-none prow p-0 justify-content-around  position-relative " id='projects'>
 
